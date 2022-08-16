bb37c86f741e6a98304732686ef8adaa52754385
s bacha <sam@manifoldfinance.com>
2022-08-16 10:42:55 -0700

    fixup (#26)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,13 @@
-
-
 'use strict';
 
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
   endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
   printWidth: 100,
+  proseWrap: 'always',
   quoteProps: 'as-needed',
   semi: true,
   singleQuote: true,
